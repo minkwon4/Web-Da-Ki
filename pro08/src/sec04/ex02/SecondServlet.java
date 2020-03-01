63289330c53fa7cc566cbb36684b02690b4c9bb6
unknown <minkwon4@naver.com>
2020-03-02 01:28:58 +0900

    2020/03/02 chap08

---
+++ pro08/src/sec04/ex02/SecondServlet.java
@@ -0,0 +1,32 @@
+package sec04.ex02;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FirstServlet
+ */
+@WebServlet("/second")
+public class SecondServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+
+		String add=(String)request.getAttribute("address");
+		out.println("<html><body>");
+		out.println("주소"+add);
+		out.println("</body></html>");
+	}
+
+}
