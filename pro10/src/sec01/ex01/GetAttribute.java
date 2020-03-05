8a1c57f7dfe892f12a9418990c52c39dc5784849
unknown <minkwon4@naver.com>
2020-03-05 21:19:28 +0900

    2020/03/05 chap10

---
+++ pro10/src/sec01/ex01/GetAttribute.java
@@ -0,0 +1,34 @@
+package sec01.ex01;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class GetAttribute
+ */
+@WebServlet("/get")
+public class GetAttribute extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out = response.getWriter();
+		ServletContext ctx = getServletContext();
+		HttpSession sess = request.getSession();
+		
+		String ctxMesg = (String)ctx.getAttribute("context");
+		String sesMesg = (String)sess.getAttribute("session");
+		String reqMesg = (String)ctx.getAttribute("request");
+	
+		out.println("context 값 :"+ctxMesg+"<br>");
+		out.println("session 값 :"+sesMesg+"<br>");
+		out.println("request 값 :"+reqMesg+"<br>");
+	}
+
+}
